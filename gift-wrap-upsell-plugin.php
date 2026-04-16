e38473f1204d4e8ab73e06272417f7de677796b2
digvijay-wisetr <digvijay.s@wisetr.com>
2026-04-16 12:38:45 +0530

    inital plugin setup

---
+++ gift-wrap-upsell-plugin.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Gift Wrap Upsell Plugin
+ * Description: Adds gift wrap upsell functionality.
+ * Version: 1.0.0
+ * Author: Digvijay Singh
+ * Text Domain: gift-wrap-upsell-plugin
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Prevent direct access
+}
